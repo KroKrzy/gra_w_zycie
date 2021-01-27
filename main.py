2c4101ea028ade7e18e2405aeb15ae490ec4de6d
KroKrzy <kkrok.cisco2017@gmail.com>
2021-01-27 22:28:39 +0100

    Kolejny commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from komorka import Komorka
 from plansza import Plansza
-
+import time
 
 n = int(input("Podaj szerokosc planszy: "))
 m = int(input("Podaj wysokosc planszy: "))
@@ -9,17 +9,18 @@ field = Plansza(n, m)
 
 plansza = [[Komorka(i,j) for j in range(m+2)] for i in range(n+2)]
 
+field.drawGrid()
+
 while (zapis):
     field.input()
-
+    
 
 
 while (True):
-
+    field.work()
     for i in range (1,n+1):
         for j in range (1,m+1):
             plansza[i][j].liczsasiadow()
 
-    for i in range (1,n+1):
-        for j in range (1,m+1):
-            plansza[i][j].czy_zyje()
+    field.draw()
+    time.sleep(0.5)
--- plansza.py
+++ plansza.py
@@ -22,18 +22,21 @@ class Plansza:
         mousePos = (x, y)
         return mousePos
 
-    def drawRect(self):
-        wsp = self.mouse_position()
-        r = pygame.Rect((wsp[0], wsp[1]), (self.rozmiar_Kratki, self.rozmiar_Kratki))
+    def drawRect(self,poz):
+        poz = [poz[0] * 15,poz[1] * 15]
+        
+        r = pygame.Rect((poz[0], poz[1]), (self.rozmiar_Kratki, self.rozmiar_Kratki))
         pygame.draw.rect(self.screen, (255, 255, 255), r)
-
+        pygame.display.flip()
     def drawGrid(self):
         for x in range(self.wysokosc_Okna):
             for y in range(self.szerokosc_Okna):
                 rect = pygame.Rect(x * self.rozmiar_Kratki, y * self.rozmiar_Kratki,
                                    self.rozmiar_Kratki, self.rozmiar_Kratki)
                 pygame.draw.rect(self.screen, (200, 200, 200), rect, 1)
-
+        pygame.display.flip()
+        pygame.image.save(self.screen, 'temp.jpeg')
+        
     def input(self):
         global zapis
         for event in pygame.event.get():
@@ -42,19 +45,22 @@ class Plansza:
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 sys.exit(0)
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                self.drawRect()
+                self.drawRect(self.mouse_position())
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 zapis = False
 
+    def work(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
 
-field = Plansza()
-field.drawGrid()
-pygame.display.flip()
-pygame.image.save(field.screen, 'temp.jpeg')
-myimage = pygame.image.load("temp.jpeg").convert_alpha()
-imagerect = myimage.get_rect()
-
-while True:
-
-    field.input()
-    pygame.display.flip()
+    def draw(self):
+        global plansza
+        self.screen.fill((0,0,0))
+        myimage = pygame.image.load("temp.jpeg").convert_alpha()
+        self.screen.blit(myimage,myimage.get_rect())
+        
+        for i in range (self.wysokosc_Okna):
+            for j in range(self.szerokosc_Okna):
+                if plansza[i][j].zyje:
+                    self.drawRect([i,j])        
